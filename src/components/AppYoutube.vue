c3372e4b58ff628fa622a77e510fa0d93b1677ce
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-04-23 00:13:58 +0200

    fix: youtube

---
+++ src/components/AppYoutube.vue
@@ -0,0 +1,54 @@
+<script>
+    export default {
+        name: 'AppYoutube',
+
+        data(){
+            return{
+                
+            }
+        },
+        methods: {
+            
+            }
+        }
+	
+
+    
+</script>
+
+<template>
+    <section class="py-4" >
+        <div class="container">
+            <div class="row ">
+                <div class="col-12 d-flex justify-content-center position-relative">
+                    <img src="../assets/img/artist-video-poster.jpg" alt="">
+                    <img  class="youtube position-absolute" src="../assets/img/icon-youtube-play.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/variables' as *;
+
+section{
+    background-image: url('../assets/img/maxcoach-shape-05.png'), url('../assets/img/maxcoach-shape-12.png') ;
+    background-repeat: no-repeat;
+    background-position: 18% -2%, 88% 100%
+   
+}
+
+.col-12{
+    img{
+        box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+    }
+}
+
+.youtube{
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+</style>
