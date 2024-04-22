b20ff47f3b05bb3fcb8e78d368337e263b61c97b
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-04-22 11:34:53 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import { store } from './store.js';
+import AppHeader from './components/AppHeader.vue';
+  
+
+export default {
+  components: {
+    AppHeader,
+
+  },
+
+  data() {
+    return {
+      store
+    };
+  },
+  
+  methods: {
+   
+  },
+  mounted() {
+   
+  }
+}
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+</template>
+
+<style>
+
+</style>
+
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+    export default {
+        name: 'AppHeader',
+
+        data(){
+            return{
+                
+            }
+        },
+        methods: {
+            
+            }
+        }
+	
+
+    
+</script>
+
+<template>
+poccia
+</template>
+
+<style scoped lang="scss">
+
+</style>
