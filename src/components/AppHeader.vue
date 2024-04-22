e3cfc9e499c8dc2f9c36d576f11e13101c825ee6
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-04-22 12:03:51 +0200

    feat: add header top

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -17,9 +17,30 @@
 </script>
 
 <template>
-poccia
+    <div class="container-fluid">
+        <div class="row header-top">
+            <div class="col-12 d-flex py-2 justify-content-center align-items-center gap-4">
+                Starts TOMORROW! Our bigget event of the year...
+            <span class="fw-bold" ><i class="fa-regular fa-clock"></i> 00 : 00 : 00: 00</span>
+            <button class="btn fw-bold text-white px-4">Get Started</button>
+            </div>
+        </div>
+        <div class="row"></div>
+    </div>
 </template>
 
 <style scoped lang="scss">
+@use '../style/partials/variables' as *;
 
+.header-top{
+    background-color: #F5F5F5;
+
+        .btn{
+            background-color: $brand-secondary-color;
+        }
+}
+
+.header-top{
+    
+}
 </style>
